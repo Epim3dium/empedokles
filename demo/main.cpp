9926bbd82763d320793afb9a6bc5f9db370974d6
Epim3dium <Epim3dium@protonmail.com>
2024-06-22 22:28:40 +0200

    changes in demo due to componentSystem

--- demo/main.cpp
+++ demo/main.cpp
@@ -11,47 +11,19 @@
 #include "physics/rigidbody.hpp"
 using namespace emp;
 
-struct Point {
-    vec2f pos;
-    vec2f last_pos;
-    vec2f force;
-    float mass;
-    static constexpr float radius = 5.f;
-    Point(vec2f p, float m = 1.f) : pos(p), last_pos(p), force(0, 0), mass(m) {}
-};
-void integrate(Point& p, float deltaTime) {
-    vec2f acc = p.force / p.mass;
-    vec2f prev = p.pos;
-    p.pos = 2.f * p.pos - p.last_pos + acc * (deltaTime * deltaTime);
-    p.last_pos = prev;
-    p.force = vec2f(0, 0);
-}
-void resolveCollision(Point& p1, Point& p2, float deltaTime) {
-    auto ql = qlen(p1.pos - p2.pos);
-    auto rad_sum = p1.radius + p2.radius;
-    if(ql < rad_sum * rad_sum) {
-        auto l = sqrt(ql);
-
-        auto mag = l - rad_sum;
-        mag /= (p1.mass + p2.mass);
-        auto dir = (p1.pos - p2.pos) / l;
-        p1.pos -= dir * mag * p2.mass * 0.5f;
-        p2.pos += dir * mag * p1.mass * 0.5f;
-    }
-}
 struct Body {
-    Transform transform;
-    Collider collider;
-    Rigidbody rigidbody;
-    Material material;
+    TransformInstance transform;
+    ColliderInstance collider;
+    RigidbodyInstance rigidbody;
+    MaterialInstance material;
     Body(std::vector<vec2f> point_cloud, float density = 1.f)  :
         transform(calculateMassInertiaArea(point_cloud).centroid),
         collider(point_cloud, &transform, true),
-        rigidbody(&transform, &collider, density),
+        rigidbody(&transform, collider.ptr(), density),
         material()
     {
-        EMP_LOG_DEBUG << "created body; mass: " << rigidbody.mass() << "\tinertia: " << rigidbody.inertia()
-            << "\tat: " << transform.position.x << ", " << transform.position.y;
+        EMP_LOG_DEBUG << "created body; mass: " << rigidbody.get().mass() << "\tinertia: " << rigidbody.get().inertia()
+            << "\tat: " << transform.get().position.x << ", " << transform.get().position.y;
     }
 };
 
@@ -69,13 +41,18 @@ class Demo : public App {
             std::reverse(point_cloud.begin(), point_cloud.end());
         }
         auto b = new Body(point_cloud, density);
-        phy_sys.add(&b->transform, &b->collider, &b->rigidbody, &b->material);
+        phy_sys.add(b->transform.ptr(), b->collider.ptr(), b->rigidbody.ptr(), b->material.ptr());
         return b;
     }
     bool setup(sf::Window& window) override {
+        ColliderSystem::allocateMemory();
+        RigidbodySystem::allocateMemory();
+        TransformSystem::allocateMemory();
+        MaterialSystem::allocateMemory();
+
         Body* platform = createBody({window_size, vec2f(10.f, window_size.y), vec2f(10.f, window_size.y - 50.f),
                                    vec2f(window_size.x, window_size.y - 50.f)}, INFINITY);
-        platform->rigidbody.isStatic = true;
+        platform->rigidbody.get().isStatic = true;
         bodies.push_back(platform);
 
         return true;
@@ -97,14 +74,14 @@ class Demo : public App {
         }
         if(keys[sf::Keyboard::Tab].pressed) {
             auto body = createBody(points, INFINITY);
-            body->rigidbody.isStatic = true;
+            body->rigidbody.get().isStatic = true;
             bodies.push_back(body);
             points.clear();
         }
         for(auto b : bodies) {
-            b->transform.update();
-            b->collider.update();
-            b->rigidbody.update();
+            b->transform.get().update();
+            b->collider.get().update();
+            b->rigidbody.get().update();
         }
         phy_sys.substeps(dt.asSeconds(), gravity);
 
@@ -118,26 +95,18 @@ class Demo : public App {
             cs.setPosition(p);
             window.draw(cs);
         }
-        {
-            cs.setFillColor(sf::Color::Magenta);
-            for(auto p : phy_sys.contactpoints ) {
-                cs.setPosition(p);
-                window.draw(cs);
-            }
-            phy_sys.contactpoints.clear();
-        }
 
         sf::Color color = sf::Color::Green;
 
         for(auto bptr : bodies) {
-            auto& b_many = bptr->collider.constituentConvex();
+            auto& b_many = bptr->collider.get().constituentConvex();
             for(auto b : b_many) {
                 auto prev = b.back();
                 for(auto p : b) {
                     sf::Vertex verts[2];
                     verts[0].position = prev;
                     verts[1].position = p;
-                    if(bptr->rigidbody.isStatic) {
+                    if(bptr->rigidbody.get().isStatic) {
                         verts[0].color = sf::Color::Yellow;
                         verts[1].color = sf::Color::Yellow;
                     }
@@ -151,10 +120,10 @@ class Demo : public App {
 
 int main()
 {
-    Demo demo;
-    run(demo);
-
-
+    {
+        Demo demo;
+        run(demo);
+    }
     return 0;
 }
 
