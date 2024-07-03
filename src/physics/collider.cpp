85b31041b95b684bf7f709c86f19491b97a46889
Epim3dium <Epim3dium@protonmail.com>
2024-07-03 20:22:19 +0200

    moved most of the logic out of collider and into collider system

--- src/physics/collider.cpp
+++ src/physics/collider.cpp
@@ -1,38 +1,58 @@
 #include "collider.hpp"
+#include "core/coordinator.hpp"
 #include "math/geometry_func.hpp"
+#include "debug/debug.hpp"
 namespace emp {
-    Collider::Collider(std::vector<vec2f> shape, emp::Transform* trans, bool correctCOM) {
-        auto MIA = calculateMassInertiaArea(shape);
-        m_inertia_dev_mass = MIA.MMOI;
-        m_area = MIA.area;
-        if(correctCOM) {
-            for(auto& p : shape) {
+    // Collider::Collider(std::vector<vec2f> shape, emp::Transform* trans, bool correctCOM) {
+    //     auto MIA = calculateMassInertiaArea(shape);
+    //     inertia_dev_mass = MIA.MMOI;
+    //     area = MIA.area;
+    //     if(correctCOM) {
+    //         for(auto& p : shape) {
+    //             p -= MIA.centroid;
+    //         }
+    //     }
+    //     model_outline = shape;
+    //     transformed_outline = model_outline;
+    //
+    //     auto triangles = triangulateAsVector(shape);
+    //     model_shape = mergeToConvex(triangles);
+    //     transformed_shape = model_shape;
+    //     m_transform = trans;
+    // }
+    void ColliderSystem::update() {
+        for(auto entity : entities) {
+            auto& transform = coordinator.getComponent<Transform>(entity);
+            auto& collider = coordinator.getComponent<Collider>(entity);
+            for(int i = 0; i < collider.model_shape.size(); i++) {
+                auto& poly = collider.model_shape[i];
+                for(int ii = 0; ii < poly.size(); ii++) {
+                    collider.transformed_shape[i][ii] = transform.globalTransform().transformPoint(poly[ii]);
+                }
+            }
+            for (int i = 0; i < collider.model_outline.size(); i++) {
+                collider.transformed_outline[i] = transform.globalTransform().transformPoint(collider.model_outline[i]);
+            }
+        }
+    }
+#define EMP_CORRECT_COM true
+    void ColliderSystem::onEntityAdded(Entity entity) {
+        auto& transform = coordinator.getComponent<Transform>(entity);
+        auto& collider = coordinator.getComponent<Collider>(entity);
+
+        EMP_LOG(DEBUG2) << "added collider to ColliderSystem";
+        auto MIA = calculateMassInertiaArea(collider.model_outline);
+        collider.inertia_dev_mass = MIA.MMOI;
+        collider.area = MIA.area;
+        if(EMP_CORRECT_COM) {
+            for(auto& p : collider.model_outline) {
                 p -= MIA.centroid;
             }
         }
-        m_model_outline = shape;
-        m_transformed_outline = m_model_outline;
+        collider.transformed_outline = collider.model_outline;
 
-        auto triangles = triangulateAsVector(shape);
-        m_model_shape = mergeToConvex(triangles);
-        m_transformed_shape = m_model_shape;
-        m_transform = trans;
-    }
-    void Collider::update() {
-        if(m_transform == nullptr) {
-            m_transformed_outline = m_model_outline;
-            m_transformed_shape = m_model_shape;
-            return;
-        }
-
-        for(int i = 0; i < m_model_shape.size(); i++) {
-            auto& poly = m_model_shape[i];
-            for(int ii = 0; ii < poly.size(); ii++) {
-                m_transformed_shape[i][ii] = m_transform->globalTransform().transformPoint(poly[ii]);
-            }
-        }
-        for (int i = 0; i < m_model_outline.size(); i++) {
-            m_transformed_outline[i] = m_transform->globalTransform().transformPoint(m_model_outline[i]);
-        }
+        auto triangles = triangulateAsVector(collider.model_outline);
+        collider.model_shape = mergeToConvex(triangles);
+        collider.transformed_shape = collider.model_shape;
     }
 };
--- src/physics/collider.hpp
+++ src/physics/collider.hpp
@@ -1,55 +1,30 @@
 #ifndef EMP_COLLIDER_HPP
 #define EMP_COLLIDER_HPP
-#include "core/component_system.hpp"
+#include "core/system.hpp"
 #include "math/math_defs.hpp"
-#include "core/transform.hpp"
+#include "scene/transform.hpp"
 #include <vector>
 namespace emp {
 class Collider;
 
-typedef ComponentSystem<Collider> ColliderSystem;
-typedef ComponentInstance<Collider, ColliderSystem> ColliderInstance;
 
-class Collider {
+struct Collider {
     typedef std::vector<vec2f> ConvexVertexCloud;
-private:
     //potentially concave
-    std::vector<vec2f> m_model_outline;
-    std::vector<ConvexVertexCloud> m_model_shape;
+    std::vector<vec2f> model_outline;
+    std::vector<ConvexVertexCloud> model_shape;
 
-    std::vector<vec2f> m_transformed_outline;
-    std::vector<ConvexVertexCloud> m_transformed_shape;
+    std::vector<vec2f> transformed_outline;
+    std::vector<ConvexVertexCloud> transformed_shape;
 
-    emp::Transform* m_transform;
-    float m_area;
-    float m_inertia_dev_mass;
-
-    void m_calcModelShape();
+    float area;
+    float inertia_dev_mass;
+};
+//system for updating transfomred collider shapes
+class ColliderSystem : public SystemOf<Transform, Collider> {
 public:
-    inline const std::vector<vec2f>& outlineModel() const {
-        return m_model_outline;
-    }
-    inline const std::vector<ConvexVertexCloud>& constituentConvexModel() const {
-        return m_model_shape;
-    }
-    inline const std::vector<vec2f>& outline() const {
-        return m_transformed_outline;
-    }
-    inline const std::vector<ConvexVertexCloud>& constituentConvex() const {
-        return m_transformed_shape;
-    }
-
-    inline float area() const { return m_area; }
-    inline float inertiaDevMass() const { return m_inertia_dev_mass; }
-
     void update();
-    
-    Collider(const Collider&) = delete;
-    Collider(Collider&&) = delete;
-    Collider& operator=(const Collider&) = delete;
-private:
-    Collider(std::vector<vec2f> shape, emp::Transform* trans, bool correctCOM = false);
-    friend ColliderSystem;
+    void onEntityAdded(Entity entity) override;
 };
 };
 #endif
