57c97cf4ce250afbcf704779268b66555d390c4e
Epim3dium <Epim3dium@protonmail.com>
2024-07-03 11:50:31 +0200

    basic elements of ecs

---
+++ src/core/entity.hpp
@@ -0,0 +1,9 @@
+#ifndef EMP_ENTITY_HPP
+#define EMP_ENTITY_HPP
+#include <cassert>
+#include <cstdint>
+namespace emp {
+    typedef uint32_t Entity;
+    const Entity MAX_ENTITIES = 4096U;
+};
+#endif
