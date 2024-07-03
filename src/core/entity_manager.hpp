406b32e726af3b3fb81c7ce70eb9d6c0c8e79eaf
Epim3dium <Epim3dium@protonmail.com>
2024-07-03 11:50:58 +0200

    component and entity manager

---
+++ src/core/entity_manager.hpp
@@ -0,0 +1,30 @@
+#ifndef EMP_ENTITY_MANAGER_HPP
+#define EMP_ENTITY_MANAGER_HPP
+#include "core/component.hpp"
+#include <cassert>
+#include <array>
+#include <cstdint>
+#include <queue>
+#include "entity.hpp"
+namespace emp {
+    class EntityManager
+    {
+        public:
+            EntityManager();
+
+            Entity createEntity();
+            void destroyEntity(Entity entity);
+            void setSignature(Entity entity, Signature signature);
+            Signature getSignature(Entity entity) const;
+        private:
+            // Queue of unused entity IDs
+            std::queue<Entity> m_available_entities{};
+
+            // Array of signatures where the index corresponds to the entity ID
+            std::array<Signature, MAX_ENTITIES> m_signatures;
+
+            // Total living entities - used to keep limits on how many exist
+            uint32_t m_living_entity_count{};
+    };
+};
+#endif
