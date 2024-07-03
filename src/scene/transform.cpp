9f08d14585e0748c92148fb2e17d5d960ab6ed2b
Epim3dium <Epim3dium@protonmail.com>
2024-07-03 21:22:23 +0200

    moved most of the logic of transform into transform system

--- src/scene/transform.cpp
+++ src/scene/transform.cpp
@@ -1,4 +1,5 @@
 #include "transform.hpp"
+#include "core/coordinator.hpp"
 #include "debug/log.hpp"
 
 namespace emp {
@@ -25,31 +26,11 @@ namespace emp {
         scale = s;
         m_syncWithChange();
     }
-    void Transform::addChild(Transform* child) {
-        assert(child->m_parent == nullptr);
-        m_children.push_back(child);
-        child->m_parent = this;
-    }
-    void Transform::removeChild(Transform* child) {
-        auto itr = std::find(m_children.begin(), m_children.end(), child);
-        if(itr != m_children.end()) {
-            child->m_parent = nullptr;
-            m_children.erase(itr);
-        }
-    }
-    void Transform::setParent(Transform* parent) {
-        if(m_parent != nullptr) {
-            m_parent->removeChild(this);
-        }
-        parent->addChild(this);
-    }
-    void Transform::update() {
-        m_updateLocalTransform();
-        if(m_parent != nullptr) {
-            m_global_transform = m_parent->globalTransform();
-            m_global_transform.combine(m_local_transform);
-        }else {
-            m_global_transform = m_local_transform;
+    void TransformSystem::update() {
+        for(auto entity : entities) {
+            auto& trans = coordinator.getComponent<Transform>(entity);
+            trans.m_updateLocalTransform();
+            trans.m_global_transform = trans.m_local_transform;
         }
     }
 };
