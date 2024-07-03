f08bda83f8957f1b17c90725b6e23f43f3f13a2a
Epim3dium <Epim3dium@protonmail.com>
2024-07-03 20:22:26 +0200

    removed bloat

--- src/physics/material.hpp
+++ src/physics/material.hpp
@@ -1,20 +1,12 @@
 #ifndef EMP_MATERIAL_HPP
 #define EMP_MATERIAL_HPP
-#include "core/component_system.hpp"
 namespace emp {
 class Material;
-typedef ComponentSystem<Material> MaterialSystem;
-typedef ComponentInstance<Material, MaterialSystem> MaterialInstance;
 struct Material {
     float static_friction = 0.4;
     float dynamic_friction = 0.2;
     float restitution = 0.1;
     float reserved;
-    Material(const Material&) = delete;
-    Material(Material&&) = delete;
-    Material& operator=(const Material&) = delete;
-private:
-    friend MaterialSystem;
     Material() {}
 };
 };
