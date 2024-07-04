41b97d024d4dbc75f16f377dc84576b5e8b5c52a
Epim3dium <Epim3dium@protonmail.com>
2024-07-05 00:49:41 +0200

    demo for ecs physics system

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,57 +1,83 @@
 #include "SFML/Graphics/CircleShape.hpp"
 #include "SFML/Graphics/RenderWindow.hpp"
+#include "core/coordinator.hpp"
 #include "debug/log.hpp"
 #include "math/geometry_func.hpp"
-#include "core/app.hpp"
-#include "core/transform.hpp"
+#include "scene/app.hpp"
+#include "scene/transform.hpp"
 #include "physics/material.hpp"
 #include "physics/physics_system.hpp"
 #include "physics/rigidbody.hpp"
 using namespace emp;
 
-struct Body {
-    TransformInstance transform;
-    ColliderInstance collider;
-    RigidbodyInstance rigidbody;
-    MaterialInstance material;
-    Body(std::vector<vec2f> point_cloud, float density = 1.f)  :
-        transform(calculateMassInertiaArea(point_cloud).centroid),
-        collider(point_cloud, &transform, true),
-        rigidbody(&transform, collider.ptr(), density),
-        material()
-    {
-        EMP_LOG_DEBUG << "created body; mass: " << rigidbody.get().mass() << "\tinertia: " << rigidbody.get().inertia()
-            << "\tat: " << transform.get().position.x << ", " << transform.get().position.y;
+class DrawingSystem : public SystemOf<Collider> {
+public:
+    void draw(sf::RenderTarget& target) {
+        for(const auto e : entities) {
+            auto& col = coordinator.getComponent<Collider>(e);
+            for(auto poly : col.transformed_shape) {
+                auto prev = poly.back();
+                for(auto p : poly) {
+                    sf::Vertex verts[2];
+                    verts[0].position = prev;
+                    verts[1].position = p;
+                    prev = p;
+                    target.draw(verts, 2U, sf::Lines);
+                }
+            }
+        }
     }
 };
 
 class Demo : public App {
     public:
-    std::vector<Body*> bodies;
-    PhysicsSystem phy_sys;
+    std::shared_ptr<DrawingSystem> drawing_sys;
+
+    std::shared_ptr<PhysicsSystem> physics_sys;
+    std::shared_ptr<RigidbodySystem> rigidbody_sys;
+    std::shared_ptr<ColliderSystem> collider_sys;
+    std::shared_ptr<TransformSystem> transform_sys;
     std::vector<vec2f> points;
     vec2f center = window_size  / 2.f;
     static constexpr float radius = 200;
     static constexpr float gravity = 500.f * 8.f;
 
-    Body* createBody(std::vector<vec2f> point_cloud, float density = 1.f) {
+    Entity createBody(std::vector<vec2f> point_cloud, float density = 1.f) {
         if(!isTriangulable(point_cloud)) {
             std::reverse(point_cloud.begin(), point_cloud.end());
         }
-        auto b = new Body(point_cloud, density);
-        phy_sys.add(b->transform.ptr(), b->collider.ptr(), b->rigidbody.ptr(), b->material.ptr());
-        return b;
+        auto e = coordinator.createEntity();
+        EMP_LOG_DEBUG << "new entity: " << e;
+        Transform trans(calculateMassInertiaArea(point_cloud).centroid);
+        Collider col(point_cloud, true);
+        Rigidbody rb; rb.prev_pos = trans.position;
+        rb.useAutomaticMass = true;
+        Material mat;
+
+        coordinator.addComponent(e, trans); 
+        coordinator.addComponent(e, col); 
+        coordinator.addComponent(e, rb); 
+        coordinator.addComponent(e, mat); 
+        return e;
     }
     bool setup(sf::Window& window) override {
-        ColliderSystem::allocateMemory();
-        RigidbodySystem::allocateMemory();
-        TransformSystem::allocateMemory();
-        MaterialSystem::allocateMemory();
+        coordinator.init();
 
-        Body* platform = createBody({window_size, vec2f(10.f, window_size.y), vec2f(10.f, window_size.y - 50.f),
+        coordinator.registerComponent<Transform>();
+        coordinator.registerComponent<Material>();
+        coordinator.registerComponent<Collider>();
+        coordinator.registerComponent<Rigidbody>();
+
+        transform_sys = coordinator.registerSystem<TransformSystem, Transform>();
+        drawing_sys   = coordinator.registerSystem<DrawingSystem, Collider>();
+        rigidbody_sys = coordinator.registerSystem<RigidbodySystem, Transform, Rigidbody>();
+        collider_sys  = coordinator.registerSystem<ColliderSystem,  Transform, Collider>();
+        physics_sys   = coordinator.registerSystem<PhysicsSystem,   Transform, Collider, Rigidbody, Material>();
+        Entity platform = createBody({window_size, vec2f(10.f, window_size.y), vec2f(10.f, window_size.y - 50.f),
                                    vec2f(window_size.x, window_size.y - 50.f)}, INFINITY);
-        platform->rigidbody.get().isStatic = true;
-        bodies.push_back(platform);
+        coordinator.getComponent<Rigidbody>(platform).isStatic = true;
+
+        physics_sys->gravity = 500.f;
 
         return true;
     }
@@ -66,22 +92,18 @@ class Demo : public App {
             EMP_LOG_DEBUG << "points reset";
         }
         if(keys[sf::Keyboard::Enter].pressed) {
+            EMP_LOG_DEBUG << "created body from points";
             auto body = createBody(points);
-            bodies.push_back(body);
             points.clear();
         }
         if(keys[sf::Keyboard::Tab].pressed) {
             auto body = createBody(points, INFINITY);
-            body->rigidbody.get().isStatic = true;
-            bodies.push_back(body);
             points.clear();
         }
-        for(auto b : bodies) {
-            b->transform.get().update();
-            b->collider.get().update();
-            b->rigidbody.get().update();
-        }
-        phy_sys.substeps(dt.asSeconds(), gravity);
+        rigidbody_sys->updateMasses();
+        transform_sys->update();
+        collider_sys->update();
+        physics_sys->update(*transform_sys, *collider_sys, *rigidbody_sys, dt.asSeconds());
 
     }
     void render(sf::RenderWindow& window) override {
@@ -93,35 +115,16 @@ class Demo : public App {
             cs.setPosition(p);
             window.draw(cs);
         }
+        drawing_sys->draw(window);
         EMP_DEBUGCALL(
             cs.setFillColor(sf::Color::Transparent);
             cs.setOutlineColor(sf::Color::Green);
             cs.setOutlineThickness(1.f);
-            for(auto p : phy_sys.debug_contactpoints) {
+            for(auto p : physics_sys->debug_contactpoints) {
                 cs.setPosition(p);
                 window.draw(cs);
             }
         );
-
-        sf::Color color = sf::Color::Green;
-
-        for(auto bptr : bodies) {
-            auto& b_many = bptr->collider.get().constituentConvex();
-            for(auto b : b_many) {
-                auto prev = b.back();
-                for(auto p : b) {
-                    sf::Vertex verts[2];
-                    verts[0].position = prev;
-                    verts[1].position = p;
-                    if(bptr->rigidbody.get().isStatic) {
-                        verts[0].color = sf::Color::Yellow;
-                        verts[1].color = sf::Color::Yellow;
-                    }
-                    prev = p;
-                    window.draw(verts, 2U, sf::Lines);
-                }
-            }
-        }
     }
 };
 
