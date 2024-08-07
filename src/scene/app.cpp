ba44f35f6aa2e7d1316e51eaa9c0ad4874b51f0a
Epim3dium <Epim3dium@protonmail.com>
2024-08-07 19:06:49 +0200

    removed unnnecessary models

--- src/scene/app.cpp
+++ src/scene/app.cpp
@@ -192,17 +192,17 @@ namespace emp {
 
     void App::loadAssets() {
         Texture::create(device, "../assets/textures/star.jpg", "default");
-        Model::create(device, ModelAsset::Builder().loadModel("../assets/models/bunny.obj"), "bunny");
-        auto bunny = coordinator.createEntity();
-        coordinator.addComponent(bunny, Transform(vec2f(0.f, 0.5f), 0.f, {0.5f, 0.5f}));
-        coordinator.addComponent(bunny, Model("bunny"));
+        Model::create(device, ModelAsset::Builder().loadModel("../assets/models/colored_cube.obj"), "cube");
+        auto cube = coordinator.createEntity();
+        coordinator.addComponent(cube, Transform(vec2f(0.f, 0.5f), 0.f, {0.5f, 0.5f}));
+        coordinator.addComponent(cube, Model("cube"));
         auto triangle = coordinator.createEntity();
         coordinator.addComponent(triangle, Transform(vec2f(0.f, 0.0f), 0.f, {1.0f, 1.0f}));
         auto shape = DebugShape(device, {vec2f(-0.25f, 0.f), vec2f(0.f, 0.25f), vec2f(0.25f, 0.f), vec2f(0.f, -0.25f)});
         shape.fill_color = glm::vec4(1, 0, 0, 1);
         coordinator.addComponent(triangle, shape);
-        // coordinator.addComponent(bunny, Texture("default"));
-        EMP_LOG_DEBUG << "bunny created, id: " << bunny;
+        // coordinator.addComponent(cube, Texture("default"));
+        EMP_LOG_DEBUG << "cube created, id: " << cube;
     }
 
 
