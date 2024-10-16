b320a1a87e2cace72cfb4d9331ff12b3266667d7
Epim3dium <Epim3dium@protonmail.com>
2024-10-16 20:05:28 +0100

    new layer class

---
+++ src/core/layer.hpp
@@ -0,0 +1,12 @@
+#ifndef EMP_LAYER_HPP
+#define EMP_LAYER_HPP
+#include <bitset>
+namespace emp {
+
+const unsigned int MAX_LAYERS = 32;
+typedef std::uint8_t Layer;
+const Layer INVALID_LAYER = UINT8_MAX;
+typedef std::bitset<MAX_LAYERS> LayerMask;
+
+};
+#endif //EMP_LAYEr_HPP
