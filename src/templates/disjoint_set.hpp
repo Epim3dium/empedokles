cd4b89758a99b95eaa4c85435a9a5cb1f5d2fa05
Epim3dium <Epim3dium@protonmail.com>
2024-10-19 16:31:22 +0100

    disjoint_set impl

---
+++ src/templates/disjoint_set.hpp
@@ -0,0 +1,42 @@
+#ifndef EMP_DISJOINT_SET_HPP
+#define EMP_DISJOINT_SET_HPP
+#include <stack>
+#include <vector>
+#include "debug/log.hpp"
+namespace emp {
+template<std::size_t Size>
+struct DisjointSet {
+    int parent[Size];
+    int rank[Size] {0};
+    int group(int element) {
+        std::stack<int> to_correct;
+        while(parent[element] != element) {
+            to_correct.push(element);
+            element = parent[element];
+        }
+        while(!to_correct.empty()) {
+            auto idx = to_correct.top();
+            parent[idx] = element;
+            to_correct.pop();
+        }
+        return element;
+    }
+    void merge(int element1, int element2) {
+        int group1 = group(element1);
+        int group2 = group(element2);
+
+        int bigger_group  = (rank[group1] > rank[group2] ? group1 : group2);
+        int smaller_group = (bigger_group == group1 ? group2 : group1);
+
+        rank[bigger_group] += rank[smaller_group] + 1;
+        parent[smaller_group] = bigger_group;
+    }
+    DisjointSet() {
+        for(int i = 0; i < Size; i++) {
+            parent[i] = i;
+        }
+    }
+
+};
+};
+#endif // EMP_DISJOINT_SET_HPP
