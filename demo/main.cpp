4eade1fe69e98d5af477ee96514758c798e9645b
Epim3dium <Epim3dium@protonmail.com>
2024-10-20 10:38:58 +0100

    demo updates

--- demo/main.cpp
+++ demo/main.cpp
@@ -12,14 +12,24 @@ class Demo : public App {
     public:
         Entity mouse_entity;
         Entity protagonist;
-        bool isProtagonistGrounded = false;
-        static constexpr float cube_side_len = 100.f;
+        float isProtagonistGroundedSec = 0.f;
+        static constexpr float cayote_time = 0.25f;
+        static constexpr float cube_side_len = 75.f;
+        std::vector<vec2f> unit_cube = {
+                vec2f(-1.f/2.f, -1.f/2.f),
+                vec2f(-1.f/2.f, 1.f/2.f),
+                vec2f(1.f/2.f, 1.f/2.f),
+                vec2f(1.f/2.f, -1.f/2.f)
+        };
         std::vector<vec2f> cube_model_shape = {
                 vec2f(-cube_side_len/2, -cube_side_len/2),
                 vec2f(-cube_side_len/2, cube_side_len/2),
                 vec2f(cube_side_len/2, cube_side_len/2),
                 vec2f(cube_side_len/2, -cube_side_len/2)
         };
+        static constexpr int markers_count = 64;
+        Entity markers[markers_count];
+        Entity last_created_crate = 0;
         DebugShape debug_cube_shape = DebugShape(
                 device,
                 cube_model_shape,
@@ -36,6 +46,7 @@ class Demo : public App {
             : App({{"../assets/models/colored_cube.obj", "cube"}},
                 {
                     {"../assets/textures/dummy.png", "dummy"},
+                    {"../assets/textures/crate.jpg", "crate"},
                     {"../assets/textures/knight/_Run.png", "running"},
                     {"../assets/textures/knight/_Jump.png", "jump-up"},
                     {"../assets/textures/knight/_Fall.png", "jump-down"},
@@ -63,31 +74,37 @@ void Demo::onSetup(Window& window, Device& device) {
     mouse_entity = coordinator.createEntity();
     coordinator.addComponent(mouse_entity, Transform({0.f, 0.f}));
 
-    Sprite spr = Sprite(Texture("jump-down"), {cube_side_len, cube_side_len});
-    Rigidbody rb;
-    rb.useAutomaticMass = true;
-    rb.isRotationLocked = true;
-    coordinator.addComponent(protagonist, Transform(vec2f(), 0.f));
-    std::vector<vec2f> protagonist_shape = {
-            vec2f(-cube_side_len/4, -cube_side_len/2),
-            vec2f(-cube_side_len/4, cube_side_len/2),
-            vec2f(cube_side_len/4, cube_side_len/2),
-            vec2f(cube_side_len/4, -cube_side_len/2)
-    };
-    auto col = Collider(protagonist_shape);
-    col.collider_layer = PLAYER;
-    col.listen(protagonist, [&](const CollisionInfo& ci) {
-        if(dot(ci.collision_normal, vec2f(0.f, -1.f)) > 0.5f) {
-            isProtagonistGrounded = true;
-        }
-    });
+    {
+        Rigidbody rb;
+        rb.useAutomaticMass = true;
+        rb.isRotationLocked = true;
+        coordinator.addComponent(protagonist, Transform(vec2f(), 0.f));
+        std::vector<vec2f> protagonist_shape = {
+                vec2f(-100.f/4, -100.f/1.5),
+                vec2f(-100.f/4, 100.f/1.5),
+                vec2f(100.f/4, 100.f/1.5),
+                vec2f(100.f/4, -100.f/1.5)
+        };
+        auto col = Collider(protagonist_shape);
+        col.collider_layer = PLAYER;
+        col.listen(protagonist, [&](const CollisionInfo& ci) {
+            if(dot(ci.collision_normal, vec2f(0.f, -1.f)) > 0.5f) {
+                isProtagonistGroundedSec = 0.f;
+            }
+        });
 
-    coordinator.addComponent(protagonist, col);
-    coordinator.addComponent(protagonist, rb);
-    auto mat = Material(); mat.dynamic_friction *= 2.f;
-    coordinator.addComponent(protagonist, mat);
+        coordinator.addComponent(protagonist, col);
+        coordinator.addComponent(protagonist, rb);
 
-    setupAnimationForProtagonist();
+        auto mat = Material(); 
+        coordinator.addComponent(protagonist, mat);
+
+        auto db_shape = DebugShape(device, protagonist_shape, glm::vec4(1, 1, 1, 1));
+        coordinator.addComponent(protagonist, db_shape);
+
+
+        setupAnimationForProtagonist();
+    }
 
     std::pair<vec2f, float> ops[4] = {
             {vec2f(0.f, 400.0f), 0.f},
@@ -104,20 +121,26 @@ void Demo::onSetup(Window& window, Device& device) {
         coordinator.addComponent(
                 platform, DebugShape(device, cube_model_shape)
         );
+        auto col = Collider(cube_model_shape);
         col.collider_layer = GROUND;
         coordinator.addComponent(platform, col);
         coordinator.addComponent(platform, Rigidbody{true});
         coordinator.addComponent(platform, Material());
     }
+    for (auto& marker : markers) {
+        marker = coordinator.createEntity();
+        coordinator.addComponent(marker, Transform(vec2f(INFINITY, INFINITY), 0.f, {0.1f, 0.1f}));
+        coordinator.addComponent(marker, DebugShape(device, unit_cube, glm::vec4(0.8, 0.8, 1, 1)));
+    }
     debug_cube_shape.fill_color = glm::vec4(1, 1, 0, 1);
 
     coordinator.getSystem<PhysicsSystem>()->gravity = 1000.f;
-    // coordinator.addComponent(cube, Texture("default"));
+    coordinator.getSystem<PhysicsSystem>()->substep_count = 8;
 }
 
 void Demo::setupAnimationForProtagonist() {
-    auto def_size = vec2f(cube_side_len * 3.f, cube_side_len * 3.f);
-    auto offset = vec2f(0.f, -def_size.y / 3.f);
+    auto def_size = vec2f(300.f, 300.f);
+    auto offset = vec2f(10.f, -def_size.y / 3.5f);
     MovingSprite idle_moving;
     {
         Sprite idle_sprite = Sprite(Texture("idle"), def_size);
@@ -179,10 +202,10 @@ void Demo::setupAnimationForProtagonist() {
         };
         auto isFalling = [](Entity owner) {
             return coordinator.getComponent<Rigidbody>(owner)->velocity.y >
-                   0.f;
+                   25.f;
         };
         auto hasFallen = [&](Entity owner) {
-            return isProtagonistGrounded;
+            return isProtagonistGroundedSec < cayote_time;
         };
         build.addEdge("idle", "fall", isFalling);
         build.addEdge("run", "fall", isFalling);
@@ -196,10 +219,41 @@ void Demo::setupAnimationForProtagonist() {
     coordinator.addComponent(protagonist, anim_sprite);
 }
 void Demo::onFixedUpdate(const float delta_time, Window& window, KeyboardController& controller) {
-        isProtagonistGrounded = false;
 }
 void Demo::onUpdate(const float delta_time, Window& window, KeyboardController& controller) 
 {
+    isProtagonistGroundedSec += delta_time;
+    auto& phy_sys = *coordinator.getSystem<PhysicsSystem>();
+    for(auto e : phy_sys.entities) {
+        if(phy_sys.m_isDormant(e)) {
+            coordinator.getComponent<DebugShape>(e)->fill_color = {1, 0, 0, 1};
+        }else {
+            coordinator.getComponent<DebugShape>(e)->fill_color = {1, 1, 1, 1};
+        }
+    }
+    for(auto m : markers) {
+        coordinator.getComponent<Transform>(m)->position = {INFINITY, INFINITY};
+        coordinator.getComponent<Transform>(m)->scale = {1, 1};
+    }
+    int i = 0;
+    for(auto entity : phy_sys.entities) {
+        if(i == markers_count) {
+            break;
+        }
+        auto& dis_set = phy_sys.m_collision_islands;
+        auto other = dis_set.group(entity);
+
+        auto& other_trans = *coordinator.getComponent<Transform>(other);
+        auto& this_trans = *coordinator.getComponent<Transform>(entity);
+        vec2f pos1 = this_trans.position;
+        vec2f pos2 = other_trans.position;
+
+        auto& shape = *coordinator.getComponent<Transform>(markers[i++]);
+        shape.position = (pos1 + pos2) * 0.5f;
+        shape.scale = vec2f(1.f, length(pos1 - pos2));
+        shape.rotation = angle(vec2f(0.f, 1.f), pos1 - pos2);
+    }
+
     {
         coordinator.getComponent<Transform>(mouse_entity)->position =
                 controller.global_mouse_pos();
@@ -209,25 +263,30 @@ void Demo::onUpdate(const float delta_time, Window& window, KeyboardController& 
             coordinator.getComponent<Rigidbody>(protagonist)->velocity.x +=
                     controller.movementInPlane2D().x * 600.f * delta_time;
             auto& rb = *coordinator.getComponent<Rigidbody>(protagonist);
-            if (controller.get(eKeyMappings::Jump).pressed && isProtagonistGrounded) {
-                rb.velocity.y = -500.f;
+            if (controller.get(eKeyMappings::Jump).pressed && isProtagonistGroundedSec < cayote_time) {
+                isProtagonistGroundedSec = cayote_time;
+                vec2f direction = vec2f(0, -10);
+                direction.x = controller.movementInPlane2D().x;
+                direction = normal(direction);
+                rb.velocity += 500.f * direction;
             }
         }
         auto& rb = *coordinator.getComponent<Rigidbody>(protagonist);
-        auto& animated = *coordinator.getComponent<AnimatedSprite>(protagonist);
+        auto& trans = *coordinator.getComponent<Transform>(protagonist);
         bool isRunningLeft = rb.velocity.x < 0.f;
         if(abs(rb.velocity.x) > 10.f) {
-            animated.flipX = isRunningLeft;
+            trans.scale.x = isRunningLeft ?  -1.f : 1.f;
         }
     }
 
     if (controller.get(eKeyMappings::Ability1).pressed) {
-        Sprite spr = Sprite(Texture("dummy"), {cube_side_len, cube_side_len});
+        Sprite spr = Sprite(Texture("crate"), {cube_side_len, cube_side_len});
         Rigidbody rb;
         rb.useAutomaticMass = true;
         auto col = Collider(cube_model_shape);
         col.collider_layer = ITEM;
         auto entity = coordinator.createEntity();
+        last_created_crate = entity;
         coordinator.addComponent(
                 entity, Transform(vec2f(controller.global_mouse_pos()), 0.f)
         );
@@ -246,6 +305,5 @@ int main()
         demo
             .run();
     }
-    return 0;
 }
 
