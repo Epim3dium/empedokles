40193094141d4a9af268ed7a6690c7870a20ef8e
Epim3dium <Epim3dium@protonmail.com>
2024-11-14 14:51:57 +0000

    added type_pack

---
+++ src/templates/type_pack.hpp
@@ -0,0 +1,14 @@
+#ifndef EMP_TYPE_PACK_HPP
+#define EMP_TYPE_PACK_HPP
+#include <cstddef>
+#include <tuple>
+namespace emp {
+template <typename... Types>
+struct TypePack {
+    static constexpr std::size_t size = sizeof...(Types);
+
+    template <std::size_t Index>
+    using Type = std::tuple_element_t<Index, std::tuple<Types...>>;
+};
+};
+#endif //EMP_TYPE_PACK_HPP
