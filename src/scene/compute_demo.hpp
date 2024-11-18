04fa2d3484ddc54168472c97afa82515e39077d9
Epim3dium <Epim3dium@protonmail.com>
2024-11-18 09:20:13 +0000

    using 2 buffers (input and output)

--- src/scene/compute_demo.hpp
+++ src/scene/compute_demo.hpp
@@ -1,5 +1,6 @@
 #pragma once 
 
+#include "debug/log.hpp"
 #include "graphics/sprite.hpp"
 #include <vulkan/vulkan_core.h>
 #include <memory>
@@ -19,16 +20,17 @@ public:
     std::unique_ptr<DescriptorSetLayout> layoutBinding;
     std::unique_ptr<DescriptorPool> compute_pool;
     const size_t dataCount = 256 * 256;
-    VkDescriptorSet descriptor_set;
+    static const uint32_t MAX_FRAMES_IN_FLIGHT = SwapChain::MAX_FRAMES_IN_FLIGHT;
+    VkDescriptorSet descriptor_set[MAX_FRAMES_IN_FLIGHT];
 
     VkPipelineLayout pipeline_layout{};
     std::unique_ptr<Pipeline> compute_pipeline;
 
-    std::unique_ptr<Buffer> dataBuffer;
+    std::unique_ptr<Buffer> dataBuffer[MAX_FRAMES_IN_FLIGHT];
     std::vector<uint32_t> inputData;
 
-    std::unique_ptr<TextureAsset> output_image;
-    TextureAsset* display_image;
+    std::unique_ptr<TextureAsset> output_image[MAX_FRAMES_IN_FLIGHT];
+    std::unique_ptr<TextureAsset> display_image[MAX_FRAMES_IN_FLIGHT];
     Transform transform;
     vec2f size = {900.f, 900.f};
     std::vector<std::unique_ptr<Buffer>> uboBuffers{
@@ -65,23 +67,25 @@ void copyImageToImage(VkCommandBuffer command_buffer,
     void initializeImages(Device& device) {
         VkExtent3D extent = {256, 256, 1};
         assert(extent.width * extent.height * extent.depth == dataCount);
-        output_image = std::make_unique<TextureAsset>(
-            device,
-            VK_FORMAT_R8G8B8A8_SRGB,
-            extent,
-            VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_STORAGE_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT,
-            VK_SAMPLE_COUNT_1_BIT);
-        display_image = &Texture::create("demo",
-            device,
-            VK_FORMAT_R8G8B8A8_SRGB,
-            extent,
-            VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT,
-            VK_SAMPLE_COUNT_1_BIT).texture();
-        {
-            auto cmd = device.beginSingleTimeCommands();
-            output_image->transitionLayout(cmd, VK_IMAGE_LAYOUT_GENERAL);
-            output_image->updateDescriptor();
-            device.endSingleTimeCommands(cmd);
+        for(int i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
+            output_image[i] = std::make_unique<TextureAsset>(
+                device,
+                VK_FORMAT_R8G8B8A8_SRGB,
+                extent,
+                VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_STORAGE_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT,
+                VK_SAMPLE_COUNT_1_BIT);
+            display_image[i] = std::make_unique<TextureAsset>(
+                device,
+                VK_FORMAT_R8G8B8A8_SRGB,
+                extent,
+                VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT,
+                VK_SAMPLE_COUNT_1_BIT);
+            {
+                auto cmd = device.beginSingleTimeCommands();
+                output_image[i]->transitionLayout(cmd, VK_IMAGE_LAYOUT_GENERAL);
+                output_image[i]->updateDescriptor();
+                device.endSingleTimeCommands(cmd);
+            }
         }
     }
     void initializeDataBuffer(Device& device) {
@@ -93,18 +97,20 @@ void copyImageToImage(VkCommandBuffer command_buffer,
             }
         }
 
-        // Create buffer
-        dataBuffer = std::make_unique<Buffer>(device,
-            sizeof(uint32_t),
-            dataCount,
-            VK_BUFFER_USAGE_STORAGE_BUFFER_BIT,
-            VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
-                VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+        for(int i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
+            // Create buffer
+            dataBuffer[i] = std::make_unique<Buffer>(device,
+                sizeof(uint32_t),
+                dataCount,
+                VK_BUFFER_USAGE_STORAGE_BUFFER_BIT,
+                VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
+                    VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
 
-        {
-            dataBuffer->map();
-            memcpy(dataBuffer->getMappedMemory(), inputData.data(), bufferSize);
-            dataBuffer->unmap();
+            {
+                dataBuffer[i]->map();
+                memcpy(dataBuffer[i]->getMappedMemory(), inputData.data(), bufferSize);
+                dataBuffer[i]->unmap();
+            }
         }
     }
     void createDescriptors(Device& device) {
@@ -114,6 +120,9 @@ void copyImageToImage(VkCommandBuffer command_buffer,
                                 VK_DESCRIPTOR_TYPE_STORAGE_BUFFER,
                                 VK_SHADER_STAGE_COMPUTE_BIT)
                             .addBinding(1,
+                                VK_DESCRIPTOR_TYPE_STORAGE_BUFFER,
+                                VK_SHADER_STAGE_COMPUTE_BIT)
+                            .addBinding(2,
                                 VK_DESCRIPTOR_TYPE_STORAGE_IMAGE,
                                 VK_SHADER_STAGE_COMPUTE_BIT)
                             .build();
@@ -121,15 +130,18 @@ void copyImageToImage(VkCommandBuffer command_buffer,
         // Descriptor pool and set allocation
         compute_pool = DescriptorPool::Builder(device)
                            .setMaxSets(SwapChain::MAX_FRAMES_IN_FLIGHT)
-                           .addPoolSize(VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1)
-                           .addPoolSize(VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, 1)
+                           .addPoolSize(VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 2)
+                           .addPoolSize(VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 2)
+                           .addPoolSize(VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, 2)
                            .build();
-        {
-            auto bufferInfoDS = dataBuffer->descriptorInfo();
+        for(int i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
+            auto bufferInfoDSIn = dataBuffer[i]->descriptorInfo();
+            auto bufferInfoDSOut = dataBuffer[1 - i]->descriptorInfo();
             DescriptorWriter(*layoutBinding, *compute_pool)
-                .writeBuffer(0, &bufferInfoDS)
-                .writeImage(1, &output_image->getImageInfo())
-                .build(descriptor_set);
+                .writeBuffer(0, &bufferInfoDSIn)
+                .writeBuffer(1, &bufferInfoDSOut)
+                .writeImage(2, &output_image[1 - i]->getImageInfo())
+                .build(descriptor_set[i]);
         }
     }
     void createPipeline(Device& device) {
@@ -198,7 +210,7 @@ void copyImageToImage(VkCommandBuffer command_buffer,
                        const Entity&) -> VkDescriptorSet {
                     auto buf_info = uboBuffers[frame_index]->descriptorInfo();
                     desc_writer.writeBuffer(0, &buf_info);
-                    auto& image_info = output_image->getImageInfo();
+                    auto& image_info = output_image[1 - frame_index]->getImageInfo();
                     desc_writer.writeImage(1, &image_info);
                     VkDescriptorSet result;
                     desc_writer.build(result);
@@ -212,23 +224,23 @@ void copyImageToImage(VkCommandBuffer command_buffer,
     }
     void performCompute(FrameInfo frame_info) {
         auto command_buffer = frame_info.commandBuffer;
+        auto frame_index = frame_info.frameIndex;
 
         {
-                display_image->transitionLayout(command_buffer,
+                display_image[frame_index]->transitionLayout(command_buffer,
                     VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
-                output_image->transitionLayout(command_buffer,
+                output_image[frame_index]->transitionLayout(command_buffer,
                     VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL);
-
         }
         copyImageToImage(command_buffer,
-            output_image->getImage(), display_image->getImage(), 256, 256, 1);
+            output_image[frame_index]->getImage(), display_image[frame_index]->getImage(), 256, 256, 1);
         {
-            display_image->transitionLayout(command_buffer,
+            display_image[frame_index]->transitionLayout(command_buffer,
                 VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
-            output_image->transitionLayout(command_buffer,
+            output_image[frame_index]->transitionLayout(command_buffer,
                 VK_IMAGE_LAYOUT_GENERAL);
         }
-        display_image->updateDescriptor();
+        display_image[frame_index]->updateDescriptor();
 
         compute_pipeline->bind(command_buffer);
         vkCmdBindDescriptorSets(command_buffer,
@@ -236,30 +248,12 @@ void copyImageToImage(VkCommandBuffer command_buffer,
             pipeline_layout,
             0, //desc set
             1, //desc count
-            &descriptor_set,
+            &descriptor_set[frame_info.frameIndex],
             0,
             nullptr);
 
         auto workgroup_count = 16;
         vkCmdDispatch(command_buffer, workgroup_count , workgroup_count , 1); // Dispatch work
-        
-        return;
-        auto& tex = *display_image;
-
-        auto pixels = tex.getPixelsFromGPU();
-        auto h = tex.getExtent().height;
-        auto w = tex.getExtent().width;
-        std::cout << "\n";
-        for(int i = 0; i < h; i += 1) {
-            for(int ii = 0; ii < w; ii += 1) {
-                auto r = (int)(pixels)[i * w + ii].red;
-                auto g = (int)(pixels)[i * w + ii].green;
-                auto b = (int)(pixels)[i * w + ii].blue;
-                std::cout << "\033[38;2;" << r << ";" << g << ";" << b << "m";
-                std::cout << "\u2588";
-            }
-            std::cout << '\n';
-        }
     }
 };
 };
