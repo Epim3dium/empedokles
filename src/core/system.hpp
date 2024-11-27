315339b6d6babf2161fa7c37c3b1ff9c05347fc4
Epim3dium <Epim3dium@protonmail.com>
2024-11-27 11:05:01 +0000

    set it so that Coordinator only uses setECS() instead of accessing private variable

--- src/core/system.hpp
+++ src/core/system.hpp
@@ -6,29 +6,26 @@
 namespace emp {
 template <typename... Components>
 class System : public SystemOf<Components...> {
-    Coordinator* m_ECS = nullptr;
     friend Coordinator;
-protected:
-    Coordinator& ECS() {
-        assert(m_ECS != nullptr && "system must be registered via coordinator");
-        return *m_ECS;
+    void setECS(Coordinator* coord) {
+        this->coordinator = coord;
     }
 public:
     template <class T>
     inline T& getComponent(Entity entity) {
         static_assert((std::is_same<T, Components>::value || ...),
                       "must get component contained in this system");
-        assert(m_ECS != nullptr && "system must be registered via coordinator");
+        assert(this->coordinator != nullptr && "system must be registered via coordinator");
         assert(this->entities.contains(entity) && "can only call getComponent on owned entities");
-        return *m_ECS->getComponent<T>(entity);
+        return *this->ECS().template getComponent<T>(entity);
     }
     template <class T>
     inline const T& getComponent(Entity entity) const {
         static_assert((std::is_same<T, Components>::value || ...),
                       "must get component contained in this system");
-        assert(m_ECS != nullptr && "system must be registered via coordinator");
+        assert(this->coordinator != nullptr && "system must be registered via coordinator");
         assert(this->entities.contains(entity) && "can only call getComponent on owned entities");
-        return *m_ECS->getComponent<T>(entity);
+        return *this->ECS().template getComponent<T>(entity);
     }
 };
 }; // namespace emp
