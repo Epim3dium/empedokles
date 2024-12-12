2711d7a0bc16d06cfe45c8d3cbae54449b23330b
Epim3dium <Epim3dium@protonmail.com>
2024-12-12 13:04:44 +0000

    correct, colorful displaying of log messages by log_window

--- src/gui/log_window.hpp
+++ src/gui/log_window.hpp
@@ -5,17 +5,15 @@
 namespace emp {
 
 class LogWindow {
-    std::vector<std::string> m_items;
-    std::vector<std::string> m_history;
-    int m_history_pos; // -1: new line, 0..History.Size-1 browsing history.
+    std::vector<std::pair<std::string, LogLevel>> m_items;
     ImGuiTextFilter m_filter;
-    bool m_isAutoScrolling;
-    bool m_isScrollingToBottom;
+    bool m_isAutoScrolling = true;
+    bool m_isScrollingToBottom = true;
 public:
     bool isOpen = true;
     void draw(const char* title);
     void clearLog();
-    void addLog(const char* fmt, ...) IM_FMTARGS(2);
+    void addLog(LogLevel level, const char* fmt, ...) IM_FMTARGS(3);
     LogWindow();
     LogWindow(const LogWindow&) = delete;
     LogWindow(LogWindow&&) = delete;
@@ -25,8 +23,9 @@ public:
 
 struct LogToGUIWindow : public LogOutput {
     LogWindow* console;
-    void Output(std::string msg) override {
-        console->addLog("%s", msg.c_str());
+    void Output(std::string msg, LogLevel level) override {
+        auto level_str = Log::ToString(level);
+        console->addLog(level, "[%s] %s", level_str.c_str(), msg.c_str());
     }
     LogToGUIWindow(LogWindow* window) : console(window) {}
 };
