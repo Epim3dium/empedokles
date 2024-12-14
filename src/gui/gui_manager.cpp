244a49bd681e129b594463b9f5e1a6047568bc89
Epim3dium <Epim3dium@protonmail.com>
2024-12-14 12:24:41 +0000

    menu for gui manager

--- src/gui/gui_manager.cpp
+++ src/gui/gui_manager.cpp
@@ -1,9 +1,39 @@
 #include "gui/gui_manager.hpp"
+#include "imgui.h"
 namespace emp  {
 
+void GUIManager::drawMainMenuBar() {
+    if (ImGui::BeginMainMenuBar())
+    {
+        if (ImGui::BeginMenu("File"))
+        {
+            if(ImGui::MenuItem("Demo", NULL, m_showDemoWindow)) m_showDemoWindow = true;
+            ImGui::EndMenu();
+        }
+        if (ImGui::BeginMenu("Edit"))
+        {
+            ImGui::EndMenu();
+        }
+        if (ImGui::BeginMenu("Tools"))
+        {
+            if(ImGui::MenuItem("Log Window", NULL, m_log_window.isOpen)) m_log_window.isOpen = true;
+            if(ImGui::MenuItem("Console", NULL, m_console.isOpen)) m_console.isOpen = true;
+            ImGui::EndMenu();
+        }
+        ImGui::EndMainMenuBar();
+    }
+}
 void GUIManager::draw(Coordinator& coordinator) {
+    drawMainMenuBar();
+    if(m_showDemoWindow)
+        ImGui::ShowDemoWindow(&m_showDemoWindow);
     m_console.draw("Console");
     m_log_window.draw("Log Window");
+    m_tree_view.draw("Tree view of entities", coordinator);
+}
+GUIManager::GUIManager() {
+    m_console.isOpen = false;
+    m_log_window.isOpen = false;
 }
 
 }
--- src/gui/gui_manager.hpp
+++ src/gui/gui_manager.hpp
@@ -4,16 +4,21 @@
 #include "core/coordinator.hpp"
 #include "gui/console.hpp"
 #include "gui/editor/inspector.hpp"
+#include "gui/editor/tree-view.hpp"
 #include "gui/log_window.hpp"
 
 namespace emp  {
 
 class GUIManager {
+    bool m_showDemoWindow = false;
+    TreeView m_tree_view;
     Console m_console;
     LogWindow m_log_window;
     void drawConsole();
+    void drawMainMenuBar();
 public:
     void draw(Coordinator& coordinator);
+    GUIManager();
 };
 
 }
