e9d9bac2f232c3c11204a8bc390cef93c2ad0b3c
Epim3dium <Epim3dium@protonmail.com>
2024-12-16 20:08:31 +0000

    building with Ninja to speed up the damn build

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir -p build
 cd build
-cmake -S ../ -B . -D CMAKE_BUILD_TYPE=$1
-make && make Shaders && ./tests/tests && $2
+cmake -S ../ -B . -D CMAKE_BUILD_TYPE=$1 -G Ninja
+cmake --build . -j && ./tests/tests && $2
 
 cd ..
 
