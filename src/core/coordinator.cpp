3cfdf2ccfa14928dff9f55777b363d94c29a805a
Epim3dium <Epim3dium@protonmail.com>
2024-12-17 11:51:20 +0000

    logging coordinator actions like registering creation, destruction

--- src/core/coordinator.cpp
+++ src/core/coordinator.cpp
@@ -1,4 +1,5 @@
 #include "coordinator.hpp"
+#include "debug/debug.hpp"
 namespace emp {
 const  Entity Coordinator::world() {
     return 0U;
@@ -14,18 +15,22 @@ void Coordinator::init() {
 
 // Entity methods
 Entity Coordinator::createEntity() {
-    return m_entity_manager->createEntity();
+    auto result = m_entity_manager->createEntity();
+    EMP_DEBUGCALL(EMP_LOG(DEBUG2) << "entity created: " << result;)
+    return result;
 }
 bool Coordinator::isEntityAlive(Entity entity) const {
     return m_entity_manager->isEntityAlive(entity);
 }
 
 void Coordinator::destroyEntity(Entity entity) {
+    EMP_DEBUGCALL(EMP_LOG(DEBUG2) << "entity destroyed: " << entity;)
     m_entity_manager->destroyEntity(entity);
     m_component_manager->EntityDestroyed(entity);
     m_system_manager->EntityDestroyed(entity);
 }
 void Coordinator::destroy() {
+    EMP_DEBUGCALL(EMP_LOG(DEBUG2) << "coordinator destroyed";)
     delete m_component_manager.release();
     delete m_entity_manager.release();
     delete m_system_manager.release();
