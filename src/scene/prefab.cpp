779d150c00d8f3ce64d08373c3f82544983b0424
Epim3dium <Epim3dium@protonmail.com>
2025-01-03 19:48:59 +0000

    basic initialization for prefab based on one entity

--- src/scene/prefab.cpp
+++ src/scene/prefab.cpp
@@ -1,1 +1,18 @@
 #include "prefab.hpp"
+#include "scene/register_scene_types.hpp"
+namespace emp {
+
+void Prefab::write(IBlobWriter& writer) {
+}
+void Prefab::read(IBlobReader& reader) {
+}
+Prefab::Prefab(Coordinator& ECS, Entity prefab_source) {
+    registerSceneTypes(environment);
+    m_mapping[prefab_source] = environment.world();
+    m_checkAndAddComponents(ECS, prefab_source);
+}
+void Prefab::m_checkAndAddComponents(Coordinator& ECS, Entity owner) {
+    m_checkAndAddFromPack(ECS, owner, AllComponentTypes());
+}
+
+}
