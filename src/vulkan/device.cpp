8b45c86ec821627eb293ee85f8d0c6e8abe00e4f
Epim3dium <Epim3dium@protonmail.com>
2025-04-11 12:42:53 +0200

    fix for building apple (missing extension)

--- src/vulkan/device.cpp
+++ src/vulkan/device.cpp
@@ -324,7 +324,9 @@ std::vector<const char*> Device::getRequiredExtensions() const {
     if (enable_validation_layers) {
         extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
     }
-    // extensions.push_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+#ifdef __APPLE__
+    extensions.push_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+#endif
     extensions.push_back(VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME);
 
     return extensions;
