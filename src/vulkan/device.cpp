0f51ed8259dfa9f3272442db9a55ec94d3b65e12
Epim3dium <Epim3dium@protonmail.com>
2025-04-17 19:17:01 +0200

    fixed all validation errors caused by PORTABILITY_EXTENSION

--- src/vulkan/device.cpp
+++ src/vulkan/device.cpp
@@ -105,7 +105,9 @@ void Device::createInstance() {
 
     VkInstanceCreateInfo createInfo = {};
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+#ifdef __APPLE__
     createInfo.flags = VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+#endif
     createInfo.pApplicationInfo = &appInfo;
 
     auto extensions = getRequiredExtensions();
