0ab81c2988777cf76d8234ca932c807d0fb3cf0d
Epim3dium <Epim3dium@protonmail.com>
2025-04-23 11:31:57 +0200

    an option for different emit shapes using emitType

--- src/graphics/particle_emit_queue.hpp
+++ src/graphics/particle_emit_queue.hpp
@@ -4,11 +4,23 @@
 #include "math/math_defs.hpp"
 namespace emp {
 static constexpr uint32_t MAX_EMIT_CALLS = 16U;
+enum EmitType {
+    POINT = 0,
+    LINE = 1,
+    RING = 2,
+};
 struct alignas(16) ParticleEmitData {
     uint32_t count; // 4, 4
-    float _pad0;// 4,8
-    vec2f position_min;// 8, 16
-    vec2f position_max;// 8, 24
+    uint32_t type; //4, 8
+    vec2f position;// 8, 16
+    union {
+        vec2f line_direction;
+        struct {
+            float ring_dist_min;
+            float ring_dist_max;
+        }ring;
+        float type_data[2];
+    }; //8, 24
 
     float speed_min;//4, 28
     float speed_max;//4, 32
@@ -28,16 +40,18 @@ struct alignas(64) EmitQueue {
     uint32_t work_start = 0; //4, 8
     uint32_t work_end = 0;   //4, 12
     uint32_t max_particles;
-    void emit(uint32_t part_count, std::pair<vec2f, vec2f> pos, std::pair<float, float> speed, std::pair<float, float> lifetime, std::pair<float, float> angle, vec4f c)
+    void emit(uint32_t part_count, EmitType type, vec2f pos, std::pair<float, float> type_data, std::pair<float, float> speed, std::pair<float, float> lifetime, std::pair<float, float> angle, vec4f c)
     {
         if(call_count >= MAX_EMIT_CALLS) {
             return;
         }
         work_end += part_count;
         auto idx = call_count++;
+        calls[idx].type = type;
+        calls[idx].type_data[0] = type_data.first;
+        calls[idx].type_data[1] = type_data.second;
         calls[idx].count = part_count;
-        calls[idx].position_min = pos.first;
-        calls[idx].position_max = pos.second;
+        calls[idx].position = pos;
         calls[idx].lifetime_min = lifetime.first;
         calls[idx].lifetime_max = lifetime.second;
         calls[idx].angle_min = angle.first;
